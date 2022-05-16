0417e5ecda2cf1b7340633380a6c4f4db3071149
shimmieblum <shimmieblum@gmail.com>
2022-05-16 23:53:30 +0100

    used blazor template and adapted to have fx exchange in it from gbp to usd

---
+++ MyRecipeApp/Data/FXResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace MyRecipeApp.Data;
+
+public class FXResponse
+{
+    public object Motd { get; set;}
+    public bool Success { get; set;}
+    public object Query { get; set;}
+    public object Info { get; set;}
+    public bool Historical { get; set;}
+    public DateTime Date { get; set;}
+    public double Result { get; set;}
+}
---
+++ MyRecipeApp/Data/FXService.cs
@@ -0,0 +1,20 @@
+﻿using System.Xml;
+using Halak;
+using Newtonsoft.Json;
+
+namespace MyRecipeApp.Data;
+
+
+public class FXService
+{
+    public static async Task<double> GetSterlingToDollarAsync(double amountToConvert)
+    {
+        using var client = new HttpClient();
+        var url = $"https://api.exchangerate.host/convert?from=GBP&to=USD&amount={amountToConvert}&places=2";
+        var response = await client.GetStringAsync(url);
+        var result = JsonConvert.DeserializeObject<FXResponse>(response);
+        if (result is null || !result.Success || result is null)
+            throw new ApplicationException("failure in fx service");
+        return result.Result;
+    }
+}
